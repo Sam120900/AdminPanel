ee746a7ac81c22edaf92dce97ea55a3c5701b4a0
Sam120900 <samindi1209@gmail.com>
2022-09-30 15:40:10 +1000

    fixed update and delete functions

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,21 +1,19 @@
 import React from "react";
-import firebase from './firebase.config'
+import db from './firebase.config'
 
 export const UserInput = ({ user }) => {
-  const [firstName, setName] = React.useState(user.firstName);
-  const [lastName, setName2] = React.useState(user.lastName);
-
-
-  const onUpdate = () => {
-    const db = firebase.firestore()
-    db.collection('userProfile').doc(user.id).set({...user, firstName})
-    db.collection('userProfile').doc(user.id).set({...user, lastName})
-  }
+    const [firstName, setName] = React.useState(user.firstName);
+    const [lastName, setName2] = React.useState(user.lastName);
+  
+  
+    const onUpdate = () => {
+      db.collection('userProfile').doc(user.id).set({...user, firstName})
+      db.collection('userProfile').doc(user.id).set({...user, lastName})
+    }
 
   const onDelete = () => {
-    const db = firebase.firestore()
     db.collection('userProfile').doc(user.id).delete()
-  }
+  }
 
   return (
     <>
